b9fda9cdc3c44ce9cb047d45d59c643bddee6d83
julixq <87141130+julixq@users.noreply.github.com>
2021-07-11 17:58:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,6 +43,13 @@ client.remove_command("help") # Coz default help sucks
 @client.event
 async def on_ready():
 	print(f"Logged in as {client.user.name}")
+@client.event
+async def on_message(message):Какая вам аватарка больше нарвится? 1 или 2
+    if message.content.startswith('!avatar):
+        embedVar = discord.Embed(title="Title", description="Desc", color=0x00ff00)
+        embedVar.add_field(name="Field1", value="hi", inline=False)
+        embedVar.add_field(name="Field2", value="hi2", inline=False)
+        await message.channel.send(embed=embedVar)
 
 # Commands
 
